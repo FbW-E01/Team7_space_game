c82a90ed110af00c209cb927e682394c779de37a
mariagluck <mariamart.berlin@gmail.com>
2021-06-08 12:00:51 +0200

    food

---
+++ food.js
@@ -0,0 +1,10 @@
+let  currentFoodPlace  =  0; // Initially set to 0
+const  createFood  = () => {
+    // Remove previous food;
+    gameBox[currentFoodPlace].classList.remove("food");
+
+    // Create new food
+    currentFoodPlace =  Math.random();
+    currentFoodPlace =  Math.floor(currentFoodPlace  *  1600);
+    gameBox[currentFoodPostion].classList.add("food"); //read-only property that returns a live DOMTokenList collection of the class attributes of the element, can then be used to manipulate the class list --> in this case to add class
+};
