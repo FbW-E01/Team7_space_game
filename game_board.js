b88aed9f3f18473016874e3e3f2c09823632b84b
Gary Dineen <dineen77@hotmail.com>
2021-06-08 13:49:49 +0200

    update

---
+++ game_board.js
@@ -0,0 +1,15 @@
+  // scoreboard//
+  let foodEaten = 0;
+  let distanceTravelled = 0 
+  
+ /// Game build///
+const gameBox = document.getElementById ("gameBox");
+
+const gameBoard = () => {
+
+  for (let i = 1; i <= 1600; ++i) {
+    gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameBoard" id="pixel"${i}"></div>`;
+  }
+};
+
+const gameBoard = document.getElementsByClassName("gameBoard");
